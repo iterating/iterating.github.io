4fe989a76ea4b12f670048c041d29f1e7268fe53
Jonathan Young <6834401+iterating@users.noreply.github.com>
2025-04-17 14:02:05 -0700

    Update client sites

--- src/components/Resume/resumeData.js
+++ src/components/Resume/resumeData.js
@@ -14,10 +14,10 @@ export const resumeData = {
         title: "Software Engineer (contract)",
         company: "LA Film Lab and Studio, Los Angeles CA",
         accomplishments: [
-          "Responsible for building a component-driven architecture using TypeScript, Nuxt.js, and Node.js with Supabase, delivering mission-critical features that improved operational efficiency and customer engagement",
-          "Developed API endpoints that retrieves and updates order information between Square's e-commerce SDK and Supabase database, processing batch transactions with error handling and real-time status tracking for customer orders",
-          "Implemented real-time event architecture with SSE, enabling instant order updates across all platforms",
-          "Developed an optimized kiosk system that reduced in-person order times by 25% and reduced manual errors",
+          "        Developed full-stack solutions with component-driven architecture using TypeScript, Vue.js, and Node.js with Supabase as the backend-as-a-service, delivering mission-critical features that drove operational efficiency and customer engagement beyond growth expectations for a team of our size",
+          "Implemented real-time pub/sub event architecture with SSE, enabling instant order updates across all platforms",
+          "Developed RESTful API endpoints that retrieves and updates order information between Square's e-commerce SDK and Supabase database, resulting in seamless order management",
+          "Developed optimized kiosks with Vue.js and TypeScript for order entry that reduced in-person order times by 25% and reduced manual errors",
           "Created database migration tools with Javascript and SQL, leading to successful database migration"
         ]
       },
@@ -25,20 +25,22 @@ export const resumeData = {
         id: 2,
         date: "2021 - 2024",
         title: "Web Developer (contract)",
-        company: "Greater Boston Snow Removal, Boston MA",
+        company: "Greater Boston Snow Removal, Boston MA (Hybrid remote)",
         accomplishments: [
-          "Developed business service platform that transformed company into Boston's market leader",
-          "Built location-based worker matching system using Google Cloud API and MongoDB geospatial indexing",
-          "Implemented microservices architecture using TypeScript and Node.js, achieving 99.9% uptime during peak snow events with 1,000+ daily transactions"
+          "Developed scalable web applications that transformed the company into Boston's market leader, growing from 4 to 80+ employees through coordinated dispatch and real-time communications",
+          "Built location-based worker matching system using Google Cloud API and MongoDB geospatial indexing, improving service time by 40%",
+          "Implemented robust security infrastructure with JWT-based authentication and role-based access control for secure data access across microservices",
+          "Deployed system on AWS with ElasticBeanstalk running EC2 instances, maintaining high availability and scalability"
         ]
       },
       {
         id: 3,
-        date: "2018 - 2020",
-        title: "Operations Manager",
+        date: "2019 - 2022",
+        title: "Business Intelligence Analyst / Operations Manager",
         company: "Greater Boston Snow Removal, Boston MA",
         accomplishments: [
-        ""
+          "Managed multiple operations teams and implemented route optimizations that reduced operational costs by 25%",
+          " Leveraged Excel, SQL, and Tableau to create dashboards for KPI tracking and business intelligence, driving strategic decision-making"
         ]
       },
       {
@@ -47,8 +49,8 @@ export const resumeData = {
         title: "Clinical User Specialist Intern",
         company: "Vecna Healthcare, Burlington MA",
         accomplishments: [
-          "Developed EMR user interfaces that reduced clinical documentation time by 35%",
-          "Reduced system deployment time by 60% through automation with Ansible",
+          "Provided usability improvements to EMR frontend with feedback from training and working with clinical staff, reducing clinical documentation time by 30%",
+          "Collaborated closely with engineering teams to prototype user interfaces with HTML, CSS, and JavaScript that meet the needs of healthcare providers and improve patient care outcomes"
         ]
       },
       {
@@ -68,23 +70,23 @@ export const resumeData = {
   educationSection: {
     sectionHeading: "Education",
     educationItems: [
-      {
-        id: 1,
-        date: "2024",
-        institution: "Per Scholas",
-        degree: "Software Engineering Immersive",
-        accomplishments: [
-          "Completed intensive training in enterprise software development practices and Agile methodologies, achieving top performance (4.0 GPA)"
-        ]
-      },
+      // {
+      //   id: 1,
+      //   date: "2024",
+      //   institution: "Per Scholas",
+      //   degree: "Software Engineering Immersive",
+      //   accomplishments: [
+      //     "Completed intensive training in enterprise software development practices and Agile methodologies, achieving top performance (4.0 GPA)"
+      //   ]
+      // },
       {
         id: 2,
-        date: "2015 - 2018",
+        date: "2014 - 2018",
         institution: "University Of Massachusetts Chan Medical School",
         degree: "M.D. Candidate",
         accomplishments: [
-          "Won American College of Physicians National Abstract Competition",
-          "Published in ACP Impact and presented at Digestive Disease Interventions",
+          "Developed clinical decision support tools and won American College of Physicians National Abstract Competition",
+          "Published in ACP Impact and presented at Digestive Disease Interventions"
         ]
       },
       {
@@ -94,17 +96,15 @@ export const resumeData = {
         degree: "B.Sc. GPA: 3.7",
         accomplishments: [
           "Graduated Magna Cum Laude with Thesis Honors",
-          "Published research in Organic and Biomolecular Chemistry",
-          "Managed IT infrastructure as Computer Lab Administrator"
+          "Managed IT infrastructure as Computer Lab Administrator at Eliot-Pearson Children's School, later earning Red Hat Certified System Administrator (RHCSA) certification"
         ]
       },
       {
         id: 4,
         date: "",
         institution: "Choate Rosemary Hall",
-        degree: "",
+        degree: "American Invitational Mathematics Examination (AIME) Qualifier",
         accomplishments: [
-          "American Invitational Mathematics Examination (AIME) Qualifier",
           "Recognized for exceptional problem-solving  skills"
         ]
       }
@@ -113,43 +113,43 @@ export const resumeData = {
   skillsSection: {
     sectionHeading: "Skills",
     skills: [
-      "Typescript/Javascript",
-      "LangChain",
+      "Typescript/Javascript, Node.js",
       "Python, Flask, Numpy, Scipy",
       "GraphQL, RESTful web services",
-      "PostgreSQL, MySQL, MongoDB",
+      "SQL skill certification, PostgresSQL, MongoDB, Firebase/Supabase",
       "React, Nuxt, Vue",
       "HTML5/CSS3",
-      "AWS, EC2, DynamoDB",
-      "Docker",
-      "Git, Linux environments",
-      "Figma, Illustrator, Photoshop",
-      "Excel, Tableau"
+      "AWS, EC2, DynamoDB, RDS, ElasticBeanstalk",
+      "LangChain, Streamlit, FastAPI",
+      "Git, Linux environments, Docker, Bash",
+      "Agile and Lean methodologies, User Stories"
+      // "Figma, Illustrator, Photoshop",
+      // "Excel, Tableau"
     ]
   },
-  projectsSection: {
-    // sectionHeading: "Projects",
-    projects: [
-      {
-        id: 1,
-        title: "Clinical Decision Support Tools",
-        description: "A collection of medical calculators and decision support tools",
-        details: [
-          "Built with JavaScript and Python",
-          "Implemented evidence-based algorithms for clinical decision making",
-          "Created intuitive user interfaces for healthcare providers"
-        ]
-      },
-      {
-        id: 2,
-        title: "Query Builder",
-        description: "Platform for data analysts and developers to execute custom queries",
-        details: [
-          "Supports PostGreSQL, MySQL, and MongoDB databases",
-          "Created data visualizer for query results",
-          "Reusable and customizable templates for data analysis"
-        ]
-      }
-    ]
-  }
+  // projectsSection: {
+  //   // sectionHeading: "Projects",
+  //   projects: [
+  //     {
+  //       id: 1,
+  //       title: "Clinical Decision Support Tools",
+  //       description: "A collection of medical calculators and decision support tools",
+  //       details: [
+  //         "Built with JavaScript and Python",
+  //         "Implemented evidence-based algorithms for clinical decision making",
+  //         "Created intuitive user interfaces for healthcare providers"
+  //       ]
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Query Builder",
+  //       description: "Platform for data analysts and developers to execute custom queries",
+  //       details: [
+  //         "Supports PostGreSQL, MySQL, and MongoDB databases",
+  //         "Created data visualizer for query results",
+  //         "Reusable and customizable templates for data analysis"
+  //       ]
+  //     }
+  //   ]
+  // }
 };
